b8237d4d77dca93026600f4d710069b7d8c7aebb
kshitij shukla <kshitijshukla202@gmail.com>
2025-12-08 19:40:10 +0530

    some modifications

--- marksavg.js
+++ marksavg.js
@@ -1,22 +1,24 @@
-let marks = [];
+// in this programme we find average of the marks of students
+let marks =[];
 for(i=0;i<5;i++){
-    marks.push(Number(prompt("enter Subject"+(i+1)+"Marks")));
+    marks.push(Number(prompt("please enter marks of subject "+(i+1))))
 }
-let sum = 0;
+console.log(marks)
+let sum =0;
 for(i=0;i<marks.length;i++){
-    sum = sum + marks[i]; 
+      sum +=marks[i]
 }
-let avg = sum/marks.length;
-console.log("the average of your marks is "+ avg);
+let avg = (sum/marks.length);
+console.log("the average of your marks is " +avg)
 if(avg>=90){
     console.log("Grade A");
 }
 else if(avg>=75 && avg<90){
-    console.log("Grade B ")
+    console.log("Grade B");
 }
-else if(avg>=40 && avg<75){
-    console.log("Grade C")
+ else if(avg>=40 && avg<75){
+    console.log("Grade C");
 }
 else{
-    console.log("fail")
+    console.log("Sorry, You Failed The Examination");
 }
